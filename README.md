0044231d1311ef2c5b250c36335c32a00db617ff
Greg Osuri <gosuri@gmail.com>
2015-11-06 20:08:28 -0800

    README: added table example

--- README.md
+++ README.md
@@ -81,6 +81,34 @@ Additional commands, type "ovrclk COMMAND --help" for more details:
 
 ## UI Components
 
+### Table Output
+
+```go
+type hacker struct {
+	Name     string
+	Birthday string
+	Bio      string
+}
+
+var hackers = []hacker{
+	{"Ada Lovelace", "December 10, 1815", "Ada was a British mathematician and writer, chiefly known for her work on Charles Babbage's early mechanical general-purpose computer, the Analytical Engine"},
+	{"Alan Turing", "23 June, 1912", "Alan was a British pioneering computer scientist, mathematician, logician, cryptanalyst and theoretical biologist"},
+}
+
+table := ui.NewTable("NAME", "BIRTHDATE", "BIO")
+table.MaxCellWidth = 20
+for _, hacker := range hackers {
+  table.AddRow(hacker.Name, hacker.Birthday, hacker.Bio)
+}
+racer.UI().Printer().Add(table).Print()
+```
+
+```sh
+NAME          BIRTHDATE         BIO
+Ada Lovelace  December 10, 1815 Ada was a British...
+Alan Turing   23 June, 1912     Alan was a Britis...
+```
+
 ### Progress Bar
 
 ```go
--- examples/table/main.go
+++ examples/table/main.go
@@ -1,24 +1,26 @@
 package main
 
 import (
+	"github.com/gosuri/racer"
 	"github.com/gosuri/racer/ui"
 )
 
 type hacker struct {
-	Name string
-	Bio  string
+	Name     string
+	Birthday string
+	Bio      string
 }
 
 var hackers = []hacker{
-	{"Ada Lovelace", "Ada was a British mathematician and writer, chiefly known for her work on Charles Babbage's early mechanical general-purpose computer, the Analytical Engine"},
-	{"Alan Turing", "Alan was a British pioneering computer scientist, mathematician, logician, cryptanalyst and theoretical biologist"},
+	{"Ada Lovelace", "December 10, 1815", "Ada was a British mathematician and writer, chiefly known for her work on Charles Babbage's early mechanical general-purpose computer, the Analytical Engine"},
+	{"Alan Turing", "23 June, 1912", "Alan was a British pioneering computer scientist, mathematician, logician, cryptanalyst and theoretical biologist"},
 }
 
 func main() {
-	table := ui.NewTable("NAME", "BIO")
+	table := ui.NewTable("NAME", "BIRTHDATE", "BIO")
 	table.MaxCellWidth = 20
 	for _, hacker := range hackers {
-		table.AddRow(hacker.Name, hacker.Bio)
+		table.AddRow(hacker.Name, hacker.Birthday, hacker.Bio)
 	}
-	ui.NewStdUI().Printer().Add(table).Print()
+	racer.UI().Printer().Add(table).Print()
 }
